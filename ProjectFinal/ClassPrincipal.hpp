0055da820420a94027d930b7744360a495b884da
R.A. Avendaño <abl.avn@gmail.com>
2019-06-09 18:59:34 -0500

    prueba primeras lineas de codigo

---
+++ ProjectFinal/ClassPrincipal.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+#ifndef _LIBRERIASDF_HPP_
+#define _LIBRERIASDF_HPP_
+class libreriaDF {
+private:
+
+public:
+	void from_csv(std::string nombreArchivo, char separacion, int header) {};
+
+};
+
+#endif // !_LIBRERIASDF_HPP_
