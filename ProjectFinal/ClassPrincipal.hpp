65879a0f1605caf6a0f1847797e31fe8be5e3dbd
R.A. Avendaño <abl.avn@gmail.com>
2019-06-13 09:22:38 -0500

    avance definición de clases del proyecto

--- ProjectFinal/ClassPrincipal.hpp
+++ ProjectFinal/ClassPrincipal.hpp
@@ -5,9 +5,18 @@
 #define _LIBRERIASDF_HPP_
 class libreriaDF {
 private:
+	size_t* arregloColumnas;
+	size_t* cantidad;
 
 public:
+	
+	libreriaDF() {};
+	~libreriaDF() {};
+
 	void from_csv(std::string nombreArchivo, char separacion, int header) {};
+	void to_csv(std::string nombreArchivo, char separacion, int header) {};
+	void indexing(int numeroColumna) {};
+	size_t seleccion() { int fila; return fila; };
 
 };
 
